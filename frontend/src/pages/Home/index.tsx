785bc604f04c19e81ddff787fe5d46877f8ba59f
Leonardo Alves <leoroberto.alves@hotmail.com>
2022-06-29 18:29:24 -0300

    feat(ContactsList): creating functionality for orderBy name

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,46 +1,89 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import {
-  Container, Header, ListContainer, Card, InputSearchContainer,
+  Container, Header, ListHeader, Card, InputSearchContainer,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
+interface Contact{
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  category_name: string;
+  category_id: string;
+}
+
+enum OrderBy {
+  ASC = 'asc',
+  DESC = 'desc'
+}
+
 export default function Home() {
+  const [contacts, setContacts] = useState<Contact[]>([]);
+  const [orderBy, setOrderBy] = useState <OrderBy>(OrderBy.ASC);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      })
+      .catch((error) => console.log(error));
+  }, [orderBy]);
+
+  function handleToggleOrderBy() {
+    setOrderBy((prevState) => (prevState === OrderBy.ASC ? OrderBy.DESC : OrderBy.ASC));
+  }
+
   return (
     <Container>
-
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato..." />
       </InputSearchContainer>
 
       <Header>
-        <strong>ContactsLists</strong>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' contato' : ' contatos'}
+        </strong>
         <a href="/new">Novo contato</a>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type="button">
-            <span>Nome</span>
-            <img src={arrow} alt="arrow" />
-          </button>
-        </header>
+      <ListHeader orderBy={orderBy}>
+        <button type="button" onClick={handleToggleOrderBy}>
+          <span>Nome</span>
+          <img src={arrow} alt="arrow" />
+        </button>
+      </ListHeader>
 
-        <Card>
+      {contacts.map((contact) => (
+        <Card key={contact.id}>
           <div className="info">
             <div className="contact-name">
-              <strong>Leonardo Alves</strong>
-              <small>Instagram</small>
+              <strong>
+                {contact.name}
+              </strong>
+              {contact.category_name && (
+              <small>
+                {contact.category_name}
+              </small>
+              )}
             </div>
-            <span>leonardo@email.com</span>
-            <span>(15)9999-9999</span>
+            <span>
+              {contact.email}
+            </span>
+            <span>
+              {contact.phone}
+            </span>
           </div>
 
           <div className="actions">
 
-            <Link to="/edit/123">
+            <Link to={`/edit/${contact.id}`}>
               <img src={edit} alt="Edit" />
             </Link>
             <button type="button">
@@ -48,7 +91,8 @@ export default function Home() {
             </button>
           </div>
         </Card>
-      </ListContainer>
+      ))}
+
     </Container>
   );
 }
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -53,22 +53,29 @@ export const Header = styled.header`
   }
 `;
 
-export const ListContainer = styled.div`
+interface ListHeaderProps{
+  orderBy: string;
+}
+
+export const ListHeader = styled.div<ListHeaderProps>`
   margin-top: 24px;
+  margin-bottom: 8px;
+  button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
 
-  header {
-    margin-bottom: 8px;
-    button {
-      background: transparent;
-      border: none;
-      display: flex;
-      align-items: center;
+    span{
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main}
+    }
 
-      span{
-        margin-right: 8px;
-        font-weight: bold;
-        color: ${({ theme }) => theme.colors.primary.main}
-      }
+    img{
+      margin-left: 8px;
+      transform: ${({ orderBy }) => (orderBy === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)')};
+      transition: transform 0.2s ease-in;
     }
   }
 `;
