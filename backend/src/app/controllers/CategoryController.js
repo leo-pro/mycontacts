c2460656b15640d9814cd3c6dc4bf1cc1d07eb05
Leonardo Alves <leoroberto.alves@hotmail.com>
2022-07-02 22:03:26 -0300

    chore(backend): improving https status codes for requests

--- backend/src/app/controllers/CategoryController.js
+++ backend/src/app/controllers/CategoryController.js
@@ -1,4 +1,4 @@
-const CategoriesRepository = require('../repositories/CategoriesRepository');
+const CategoriesRepository = require("../repositories/CategoriesRepository");
 
 class CategoryController {
   async index(request, response) {
@@ -15,7 +15,7 @@ class CategoryController {
     const category = await CategoriesRepository.findById(id);
 
     if (!category) {
-      return response.status(400).json({ error: 'Category not found' });
+      return response.status(404).json({ error: "Category not found" });
     }
 
     response.json(category);
@@ -25,12 +25,12 @@ class CategoryController {
     const { name } = request.body;
 
     if (!name) {
-      return response.status(400).json({ error: 'Name is required' });
+      return response.status(400).json({ error: "Name is required" });
     }
 
     const category = await CategoriesRepository.create({ name });
 
-    response.json(category);
+    response.status(201).json(category);
   }
 
   async update(request, response) {
@@ -38,7 +38,7 @@ class CategoryController {
     const { name } = request.body;
 
     if (!name) {
-      return response.status(400).json({ error: 'Name is required' });
+      return response.status(400).json({ error: "Name is required" });
     }
 
     const updatedCategory = await CategoriesRepository.update(id, { name });
--- backend/src/app/controllers/ContactController.js
+++ backend/src/app/controllers/ContactController.js
@@ -1,4 +1,4 @@
-const ContactsRepository = require('../repositories/ContactsRepository');
+const ContactsRepository = require("../repositories/ContactsRepository");
 
 class ContactController {
   async index(request, response) {
@@ -15,58 +15,64 @@ class ContactController {
     const contact = await ContactsRepository.findById(id);
 
     if (!contact) {
-      return response.status(400).json({ error: 'User not found' });
+      return response.status(404).json({ error: "Contact not found" });
     }
 
     response.json(contact);
   }
 
   async store(request, response) {
-    const {
-      name, email, phone, category_id,
-    } = request.body;
+    const { name, email, phone, category_id } = request.body;
 
     if (!name) {
-      return response.status(400).json({ error: 'Name is required' });
+      return response.status(400).json({ error: "Name is required" });
     }
 
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      return response.status(400).json({ error: 'This e-mail is already in use' });
+      return response
+        .status(400)
+        .json({ error: "This e-mail is already in use" });
     }
 
     const contact = await ContactsRepository.create({
-      name, email, phone, category_id,
+      name,
+      email,
+      phone,
+      category_id,
     });
 
-    response.json(contact);
+    response.status(201).json(contact);
   }
 
   async update(request, response) {
     const { id } = request.params;
-    const {
-      name, email, phone, category_id,
-    } = request.body;
+    const { name, email, phone, category_id } = request.body;
 
     const contactExists = await ContactsRepository.findById(id);
 
     if (!contactExists) {
-      return response.status(400).json({ error: 'User not found' });
+      return response.status(404).json({ error: "Contact not found" });
     }
 
     if (!name) {
-      response.status(400).json({ error: 'Name is required!' });
+      response.status(400).json({ error: "Name is required!" });
     }
 
     const contactByEmail = await ContactsRepository.findByEmail(email);
 
     if (contactByEmail && contactByEmail.id !== id) {
-      return response.status(400).json({ error: 'This e-mail is already in use' });
+      return response
+        .status(400)
+        .json({ error: "This e-mail is already in use" });
     }
 
     const contact = await ContactsRepository.update(id, {
-      name, email, phone, category_id,
+      name,
+      email,
+      phone,
+      category_id,
     });
 
     response.json(contact);
