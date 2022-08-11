e581ca3283d667f83151ea28b636f3f2beae9c8a
Leonardo Alves <leoroberto.alves@hotmail.com>
2022-08-11 18:54:42 -0300

    chore(backend): updating cors police

--- backend/src/app/middlewares/cors.js
+++ backend/src/app/middlewares/cors.js
@@ -1,5 +1,12 @@
 module.exports = (request, response, next) => {
-  response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  response.setHeader(
+    "Access-Control-Allow-Origin",
+    `${
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://mycontacts-reactjs.vercel.app/"
+    }`
+  );
   response.setHeader("Access-Control-Allow-Methods", "*");
   response.setHeader("Access-Control-Allow-Headers", "*");
   response.setHeader("Access-Control-Max-Age", "10");
