3d974e1070d3037d3ef6d4fe06d6b09154ad9c98
Leonardo Alves <leoroberto.alves@hotmail.com>
2022-08-11 18:11:43 -0300

    chore(backend): create NODE_ENV variable

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,12 +1,25 @@
 const { Client } = require("pg");
 
-const client = new Client({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+let client;
+
+if (process.env.NODE_ENV === "production") {
+  client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: true,
+  });
+} else {
+  client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+}
 
 client.connect();
 
