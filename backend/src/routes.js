a15c4480bff5d85041c095a24ab6719c9004f6b5
Leonardo Alves <leoroberto.alves@hotmail.com>
2022-08-11 19:24:28 -0300

    chore(backend): inserting a new welcome message on GET method of the API

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,20 +1,26 @@
-const { Router } = require('express');
-const CategoryController = require('./app/controllers/CategoryController');
+const { Router } = require("express");
+const CategoryController = require("./app/controllers/CategoryController");
 
-const ContactController = require('./app/controllers/ContactController');
+const ContactController = require("./app/controllers/ContactController");
 
 const router = Router();
 
-router.get('/contacts', ContactController.index);
-router.get('/contacts/:id', ContactController.show);
-router.delete('/contacts/:id', ContactController.delete);
-router.post('/contacts', ContactController.store);
-router.put('/contacts/:id', ContactController.update);
+router.get("/", (request, response) => {
+  return response.send({
+    Welcome: "MyContacts API - Made with NodeJS by https://devleo.com.br",
+  });
+});
 
-router.get('/categories', CategoryController.index);
-router.get('/categories/:id', CategoryController.show);
-router.delete('/categories/:id', CategoryController.delete);
-router.post('/categories', CategoryController.store);
-router.put('/categories/:id', CategoryController.update);
+router.get("/contacts", ContactController.index);
+router.get("/contacts/:id", ContactController.show);
+router.delete("/contacts/:id", ContactController.delete);
+router.post("/contacts", ContactController.store);
+router.put("/contacts/:id", ContactController.update);
+
+router.get("/categories", CategoryController.index);
+router.get("/categories/:id", CategoryController.show);
+router.delete("/categories/:id", CategoryController.delete);
+router.post("/categories", CategoryController.store);
+router.put("/categories/:id", CategoryController.update);
 
 module.exports = router;
