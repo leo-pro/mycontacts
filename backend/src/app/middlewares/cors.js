a46d26d1e94a36ba3cbd5a5081db5faab0daa3b9
leoroberto.alves@hotmail.com <leoroberto.alves@hotmail.com>
2023-04-09 18:23:31 -0300

    refactor(backend/cors): create a validation for allowed origins on cors middleware

--- backend/src/app/middlewares/cors.js
+++ backend/src/app/middlewares/cors.js
@@ -1,14 +1,17 @@
 module.exports = (request, response, next) => {
-  response.setHeader(
-    "Access-Control-Allow-Origin",
-    `${
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : "https://mycontacts-reactjs.vercel.app"
-    }`
-  );
-  response.setHeader("Access-Control-Allow-Methods", "*");
-  response.setHeader("Access-Control-Allow-Headers", "*");
-  response.setHeader("Access-Control-Max-Age", "10");
+  const allowedOrigins = [
+    process.env.FRONTEND_ORIGIN_URL
+  ]
+
+  const origin = request.header('origin');
+  const isAllowed = allowedOrigins.includes(origin)
+
+  if(isAllowed){
+    response.setHeader("Access-Control-Allow-Origin", origin);
+    response.setHeader("Access-Control-Allow-Methods", "*");
+    response.setHeader("Access-Control-Allow-Headers", "*");
+    response.setHeader("Access-Control-Max-Age", "10");
+  }
+
   next();
 };
